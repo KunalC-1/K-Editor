9114002b55a61dcf64f681e27d7f98ca2e48ed67
KunalC-1 <chaudhariks19.comp@coep.ac.in>
2021-03-23 08:54:09 +0530

    Added Arrow Key and other Keys

--- gui.c
+++ gui.c
@@ -5,6 +5,8 @@
 #include<sys/ioctl.h>
 #include<errno.h>
 #include<termio.h>
+#include<string.h>
+#include "gui.h"
 /* Masking 3 bits from MSB
 *    ex. ASCII Code a = 11000001 (97)
 *    =====>  C1 & 1F = 01
@@ -15,9 +17,11 @@ typedef struct Editor{
     struct termios terminal_orig;
     int screenrows;
     int screencols;
+    int x, y;
 }Editor;
 Editor E;
 
+enum keyboardKeys{ARROW_LEFT = 500, ARROW_RIGHT, ARROW_UP, ARROW_DOWN, PAGE_UP, PAGE_DOWN, HOME_KEY, END_KEY, DELETE_KEY};
 void returnError(char* s){
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
@@ -48,31 +52,118 @@ void rawModeON(){
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_terminal) == -1) returnError("Error in tcsetattr");
 }
 /*  Function Reads a character and Returns it   */
-char readCharFromTerminal(){
+int readCharFromTerminal(){
     char c;
     if(read(STDIN_FILENO, &c, 1) == -1) returnError("Error in Read");
-    return c;
+    if(c == '\x1b'){
+        char escapeSeq[3];
+        if(read(STDIN_FILENO, &escapeSeq[0], 1) != 1)   return c;
+        if(read(STDIN_FILENO, &escapeSeq[1], 1) != 1)   return c;
+           
+        if(escapeSeq[0] == '['){
+            if(escapeSeq[1] >= '0' && escapeSeq[1] <= '9'){
+                if(read(STDIN_FILENO, &escapeSeq[2], 1) != 1) return c;
+                if(escapeSeq[2] == '~'){
+                    switch(escapeSeq[1]){
+                        case '3' :
+                            return DELETE_KEY;
+                        case '5' :
+                            return PAGE_UP;
+                        case '6' :
+                            return PAGE_DOWN;
+                    }
+                }
+            }
+            else{
+                switch(escapeSeq[1]){
+                    case 'A' : 
+                        return ARROW_UP;
+                    case 'B' :
+                        return ARROW_DOWN;
+                    case 'C' :
+                        return ARROW_RIGHT;
+                    case 'D' :
+                        return ARROW_LEFT;
+                    case 'H' :
+                        return HOME_KEY;
+                    case 'F' :
+                        return END_KEY;
+                }
+            }
+        }return c;
+    }else
+        return c;
 }
+void cursorMovement(int key){
+    switch(key){
+        case ARROW_LEFT:
+            if(E.x != 0) E.x--;
+            break;
+        case ARROW_RIGHT:
+            if(E.x != E.screencols - 1) E.x++;
+            break;
+        case ARROW_UP:
+            if(E.y != 0) E.y--;
+            break;
+        case ARROW_DOWN:
+        if(E.y != E.screenrows - 1) E.y++;
+            break;
+    }
+}
+
 /*  Function Processes Key Input   */
 void processKeyInput(){
-    char c = readCharFromTerminal();
+    int repeat;
+    int c = readCharFromTerminal();
+    printf("%c" , c);
     switch (c){
-    case CTRL_KEY('q'):
-        write(STDOUT_FILENO, "\x1b[2J", 4);
-        write(STDOUT_FILENO, "\x1b[H", 3);
-        exit(0);
-        break;
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            cursorMovement(c);
+            break; 
+        case PAGE_UP:
+        case PAGE_DOWN:
+            repeat = E.screenrows;
+            c = (c == PAGE_UP) ? ARROW_UP : ARROW_DOWN;
+            while(repeat--) cursorMovement(c);
+            break;
+        case HOME_KEY :
+            E.x = 0;
+            break;
+        case END_KEY :
+            E.x = E.screencols - 1;
+            break;
     }
 }
 /*
  * Function prints out Text in PieceTable onto the screen
  */
-void displayRows(){
+void displayRows(writeBuffer* wb){
     for(int i = 0; i < E.screenrows; i++){
-        if(i != E.screenrows - 1)
-            write(STDOUT_FILENO, "~\r\n", 3);
+        // Printing Welcome message in middle of screen
+        if(i == E.screenrows/3){
+            char msg[50];
+            int msglen = sprintf(msg , "Editor By Kunal\r\n");
+            if(msglen > E.screencols) msglen = E.screencols;
+            int space = (E.screencols - msglen)/2;
+            if(space){
+                appendToWriteBuffer(wb, "~\x1b[K", 4);
+                space--;
+            }
+            while(space--) appendToWriteBuffer(wb, " ", 1);
+            appendToWriteBuffer(wb, msg, msglen);
+        }
+        else if(i != E.screenrows - 1)
+            appendToWriteBuffer(wb, "~\x1b[K\r\n", 6);
         else
-            write(STDOUT_FILENO, "~", 1);
+            appendToWriteBuffer(wb, "~\x1b[K", 4);
     }
 }
 
@@ -81,7 +172,7 @@ void displayRows(){
  */
 int getWindowSize(int *rows, int* cols){
     struct winsize ws;
-    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0 || ws.ws_row == 0)
         return -1;
     else{
         *rows = ws.ws_row;
@@ -91,23 +182,63 @@ int getWindowSize(int *rows, int* cols){
 }
 /*   Function  Refreshes the Screen     */
 void refreshScreen(){
-    // Escape Sequence for clearing whole screen
-    write(STDOUT_FILENO, "\x1b[2J", 4);
+    char position[20];
+    writeBuffer wb;
+    initWriteBuffer(&wb);
+
+    // Escape Sequence for hiding cursor
+    // appendToWriteBuffer(&wb, "\x1b[?25l", 6);
+  
     // Escape Sequence for moving cursor to initial position
-    write(STDOUT_FILENO, "\x1b[H", 3);
-    
-    displayRows();
-    
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    appendToWriteBuffer(&wb, "\x1b[H", 3);    
+
+    displayRows(&wb);
+
+    sprintf(position, "\x1b[%d;%dH", E.y+1, E.x+1);
+    appendToWriteBuffer(&wb, position, strlen(position));
+    // appendToWriteBuffer(&wb, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, wb.buf, wb.size);
+    destroyWriteBuffer(&wb);
 }
+
+/*
+ *      Write Buffer Section
+ */
+
+// Function Initializes writeBuffer ADT
+void initWriteBuffer(writeBuffer* wb){
+    wb->buf = NULL;
+    wb->size = 0;
+}
+// Function Apppend String s to writeBuffer
+void appendToWriteBuffer(writeBuffer* wb, char* s, int size){
+    char* temp = realloc(wb->buf, wb->size + size);
+    if(!temp) return;
+    memcpy(temp + wb->size, s, size);
+    wb->buf = temp;
+    wb->size += size;
+}
+// Destroys writeBuffer ADT
+void destroyWriteBuffer(writeBuffer* wb){
+    free(wb->buf);
+    wb->size = 0;
+}
+
+
+
+
 
 /*
  *      Function Initializes struct Editor
  */
 void initEditor(){
+    E.x = E.y = 0;
     if(getWindowSize(&E.screenrows, &E.screencols) == -1)
         returnError("Error in getWindowSize");
 }
+
+
 
 int main(){
     rawModeON();
---
+++ gui.h
@@ -0,0 +1,10 @@
+#ifndef GUI_H_INCLUDED
+#define GUI_H_INCLUDED
+
+typedef struct writeBuffer{
+    int size;
+    char* buf;
+}writeBuffer;
+
+
+#endif
